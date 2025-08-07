aab9acaadae818c56dc0f3be6b8e04d59df05417
callumfrederiksen <callumfrederiksen97@gmail.com>
2025-08-08 00:43:51 +0100

    Bridged upload to python

--- server/file-crawler.py
+++ server/file-crawler.py
@@ -1,8 +1,15 @@
 import os
+import pandas as pd
+
+PATH = '../uploads/'
 
 def loop():
     while True:
-        if len(os.listdir('../uploads/')) != 0:
-            return os.listdir('../uploads/')
+        if len(os.listdir(PATH)) != 0:
+            return os.listdir(PATH)
 
-print(loop())
+dir = loop()
+
+df = pd.read_csv(PATH + str(dir[0]))
+
+print(df.columns.tolist())
