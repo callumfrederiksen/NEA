cb2f8e43afc634939ab697af74a2d50221ae6ebc
callumfrederiksen <callumfrederiksen97@gmail.com>
2025-08-21 23:03:55 +0100

    OOP implementation in backend/mnist-test.py

--- backend/mnist-test.py
+++ backend/mnist-test.py
@@ -5,29 +5,41 @@ from losses import *
 import pandas as pd
 import numpy as np
 
-FILE_PATH = './datasets/mnist_train.csv'
-TRAIN_TEST_SPLIT = 0.8 # 80% train, 20% test
-SIZE = [784, 256, 128, 10]
-ACTIVATIONS = [ReLU, ReLU, Softmax]
-LOSS = CategoricalCrossEntropyWithSoftmax
-DATASET_SIZE = [785, 1]
-Y_COLUMN = 'label'
-Y_INDEX = 0 # ASSUMED
+class DataPreProcessing:
+    def __init__(self):
+        self.__FILE_PATH = './datasets/mnist_train.csv'
+        self.__TRAIN_TEST_SPLIT = 0.8  # 80% train, 20% test
+        self.__SIZE = [784, 256, 128, 10]
+        self.__ACTIVATIONS = [ReLU, ReLU, Softmax]
+        self.__LOSS = CategoricalCrossEntropyWithSoftmax
+        self.__DATASET_SIZE = [785, 1]
+        self.__Y_COLUMN = 'label'
+        self.__Y_INDEX = 0  # ASSUMED
 
-df = pd.read_csv(FILE_PATH)
-df = np.array(df)
+    def __read_csv(self):
+        df = pd.read_csv(self.__FILE_PATH)
+        df = np.array(df)
+        DF_RESHAPE = [-1] + self.__DATASET_SIZE
+        self.__df = df.reshape(DF_RESHAPE)
 
-DF_RESHAPE = [-1] + DATASET_SIZE
-df = df.reshape(DF_RESHAPE)
+    def __get_dataset_size(self):
+        self.__N_LENGTH = self.__df.shape[0]
+        return self.__N_LENGTH
 
-N_LENGTH = df.shape[0]
+    def __train_test_split(self):
+        TRAIN_VAL = int((self.__N_LENGTH * self.__TRAIN_TEST_SPLIT) // 1)
+        TRAIN_DF, TEST_DF = self.__df[:TRAIN_VAL], self.__df[TRAIN_VAL:]
 
-#Train-test-split
+        self.__train_X, self.__train_y = TRAIN_DF[:, self.__Y_INDEX + 1:], TRAIN_DF[:, self.__Y_INDEX]
+        self.__test_X, self.__test_y = TEST_DF[:, self.__Y_INDEX + 1:], TRAIN_DF[:, self.__Y_INDEX]
 
-TRAIN_VAL = int((N_LENGTH * TRAIN_TEST_SPLIT)//1)
-# TEST_VAL = N_LENGTH - TRAIN_VAL
+        return (self.__train_X, self.__train_y), (self.__test_X, self.__test_y)
 
-TRAIN_DF, TEST_DF = df[:TRAIN_VAL], df[TRAIN_VAL:]
+    def compute(self):
+        self.__read_csv()
+        self.__get_dataset_size()
+        self.__train_test_split()
 
-train_X, train_y = TRAIN_DF[:,Y_INDEX+1:], TRAIN_DF[:,Y_INDEX]
-test_X, test_y = TEST_DF[:,Y_INDEX+1:], TRAIN_DF[:,Y_INDEX]
+if __name__ == '__main__':
+    dpp = DataPreProcessing()
+    dpp.compute()
